7a2fad6ee9474e9891f4a470723c0bbb95b54daa
Hugo Neto <hugoneto@outlook.com>
2019-11-14 18:44:29 -0300

    Estrutura inicial

---
+++ T2/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        switch(args[4]){
+            // Caso Produtor
+            case "p":{
+            
+                break;
+            }
+            // Caso Consumidor
+            case "c":{
+                
+                break;
+            }
+            // Processo que nunca morre
+            case "a":{
+
+                break;
+            }
+
+        }
+
+    }
+}
