40d8effcd0c7f4383f097c97a45e36b11310e247
Hugo Neto <hugoneto@outlook.com>
2019-11-15 13:22:07 -0300

    Criaçao do nodo que nao morre e contem os dados de backup[

--- T2/Main.java
+++ T2/Main.java
@@ -1,7 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.net.InetAddress;
+
 public class Main {
+    String status = "";
+    int buffer = 10;
     public static void main(String[] args) {
         
-        switch(args[4]){
+
+        //switch(args[4]){  Linha completa de comando 
+        switch(args[0]){
             // Caso Produtor
             case "p":{
             
@@ -14,7 +22,16 @@ public class Main {
             }
             // Processo que nunca morre
             case "a":{
-
+                //Primeira linha do arquivo tem o IP do coordenador
+                try{
+                    BufferedWriter arquivo = new BufferedWriter(new FileWriter("./arq.txt"));
+                    arquivo.append("Coordenador: 192.168.1.1\n");
+                    System.out.println("Registro do Nodo que não morre Completo! \n");
+                    arquivo.close();
+                }catch(Exception ex){
+                    System.out.println("Problema na escrita do arquivo/n");
+                }
+                
                 break;
             }
 
